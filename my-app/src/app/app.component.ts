85cc0f31cdc66befcb426721ad3c088de7edabee
valexx55 <valexx55@gmail.com>
2022-04-04 21:53:24 +0200

    lunes 4

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+//"ESTO ES NUESTRO JAVASCRIPT
+//"PARA DARLE VIDILLA A LA PÁGINA
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  titulo = 'CALCULO LETRA DNI';
+  numero = 30;
+  /**
+   * qué tengo en una clase
+   * 
+   *  ATRIBUTOS/PROPIEDADES
+   *  FUNCIONES/METODOS
+   */
+
+  calcularLetraDni()
+  {
+    console.log("Boton calcular tocado");
+    console.log("Numero dni = " + this.numero);
+  }
 }
