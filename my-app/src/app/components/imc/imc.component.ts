ed23920d09b79fa2bd6ac24cb57e4745ad8eb581
valexx55 <valexx55@gmail.com>
2022-04-11 20:48:46 +0200

    lunes 11 subida 1

--- my-app/src/app/components/imc/imc.component.ts
+++ my-app/src/app/components/imc/imc.component.ts
@@ -44,7 +44,7 @@ calcularImc():void{
     this.oimc = new Imc(this.peso, this.altura);//llamamos al cosntructor
     this.array_imcs.push(this.oimc);
 
-    let imc_aux:Imc;
+    /*let imc_aux:Imc;
     for (let i=0; i<this.array_imcs.length;i++)
     { 
       imc_aux = this.array_imcs[i];
@@ -57,8 +57,17 @@ calcularImc():void{
 
     this.array_imcs.forEach(function mostar(imc_actual) {
       console.log("imc actual foreachcon funcion = " + imc_actual.altura + " " + imc_actual.peso + " "+ + imc_actual.numerico) ;
-    });
+    });*/
 
+    this.mostrarPorConsola();
+    let media_peso = this.obtenerMediaPeso(this.array_imcs);
+    console.log(`La media del peso es ${media_peso}`);
+
+    let media_altura = this.obtenerMediaAltura(this.array_imcs);
+    console.log(`La media de la altura es ${media_altura}`);
+
+    let maximo_numerico = this.obtenerMaxImc(this.array_imcs);
+    console.log(`El IMC más alto es ${maximo_numerico}`);
   }
    
   //TODO: hacer un botón en la página
@@ -75,4 +84,82 @@ borrarListaImcs()
   this.array_imcs.length=0;//es una forma de vacíar el array
 }
 
+mostrarPorConsola ():void
+{
+  this.array_imcs.forEach((imc_actual, i) => {
+    console.log(` IMC ${i} = ${imc_actual.numerico} - ${imc_actual.estado}`);
+  } );
+
+}
+
+//OPERADORES FUNCIONALES SOBRE ARRAYS
+/**
+ * forEach 
+ * filter
+ * map
+ */
+
+//TODO:
+/**
+ * Añadir estas funciones al Componente haciendo uso de 
+ * operadores funcionales 
+ * 
+ * 1) que obtenga la media del peso de los imcs
+ * 2) que obtenga la media de la estatura de los imcs
+ * 3) que me de el imc más alto
+ * 4) que filtre a los obesos
+ * 5) que sume a todos un kg
+ */
+
+
+ //1) que obtenga la media del peso de los imcs
+
+ //1 nombre
+ //2 entradas
+ //3 salidas
+ //4 completar la definición formal del método
+    //si devuelvo ALGO la primera linea, una variable/objeto del tipo devuelto
+    //si devuelvo ALGO la última línea, un return de esa variable
+
+ obtenerMediaPeso (array_caluclo: Array<Imc>):number
+ {
+   let media: number = 0;
+   let suma: number = 0;
+
+      array_caluclo.forEach( 
+      imc_temporal => 
+        { suma = suma + imc_temporal.peso; } 
+      );
+      media = suma / array_caluclo.length;
+
+   return media;
+
+ }
+
+ //2) que obtenga la media de la estatura de los imcs
+ obtenerMediaAltura (array_caluclo: Array<Imc>):number
+ {
+   let media_altura: number = 0;
+   let sumatorio: number = 0;
+
+    array_caluclo.forEach(imc_tem=> {sumatorio = sumatorio + imc_tem.altura;});
+    media_altura = sumatorio/array_caluclo.length;
+
+   return media_altura;
+ }
+
+ //3) que me de el imc más alto
+ obtenerMaxImc(array_calculo: Array<Imc>):number
+{
+  let maxImc: number = 0;
+
+    array_calculo.forEach((imc_actual) => {
+      if (imc_actual.numerico > maxImc) 
+      {
+        maxImc = imc_actual.numerico;
+      }
+    });
+  
+    return maxImc;
+}
 }
