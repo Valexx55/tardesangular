a0fa341f854cba4e34f2f856c46c9c52811e4c7d
valexx55 <valexx55@gmail.com>
2022-04-20 22:02:23 +0200

    miercoles fin

--- cursoapp/src/app/components/marcador/marcador.component.ts
+++ cursoapp/src/app/components/marcador/marcador.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Marcador } from 'src/app/models/marcador';
 
 @Component({
   selector: 'app-marcador',
@@ -8,15 +9,19 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MarcadorComponent implements OnInit {
 
 
-  marcador_usuario:number;
-  marcador_maquina:number;
+  //marcador_usuario:number;
+  //marcador_maquina:number;
+
+  marcador:Marcador;
 
   @Input() nombrejugadormarcador!:string;
 
   constructor() { 
-    this.marcador_maquina = 0;
-    this.marcador_usuario = 0;
+    //this.marcador_maquina = 0;
+    //this.marcador_usuario = 0;
     this.nombrejugadormarcador='';
+
+    this.marcador = new Marcador();
   }
 
   ngOnInit(): void {
@@ -33,23 +38,29 @@ export class MarcadorComponent implements OnInit {
     if (resultado==-1)
     {
       //gana la máquina
-      this.marcador_maquina = this.marcador_maquina +1;
+      this.marcador.puntuacion_maquina = this.marcador.puntuacion_maquina +1;
       
     } else if (resultado==0)
     {
-      this.marcador_maquina = this.marcador_maquina +1;
-      this.marcador_usuario = this.marcador_usuario +1;
+      this.marcador.puntuacion_maquina = this.marcador.puntuacion_maquina +1;
+      this.marcador.puntuacion_jugador = this.marcador.puntuacion_jugador +1;
     }else 
     {
       //gana el usuario
-      this.marcador_usuario = this.marcador_usuario +1;
+      this.marcador.puntuacion_jugador = this.marcador.puntuacion_jugador +1;
      
     }
   }
 
+
+  //TODO: GUARDAR EL RESULTADO DE LA PARTIDA
+  //PARA QUE CUANDO ENTRE, ME APREZCA
+
+  
+
   sumarMarcadorMaquina()
   {
-    this.marcador_maquina++;
+    //this.marcador_maquina++;
   }
 
 }
---
+++ cursoapp/src/app/models/marcador.ts
@@ -0,0 +1,12 @@
+export class Marcador {
+
+    puntuacion_jugador:number;
+    puntuacion_maquina:number;
+
+    constructor()
+    {
+        this.puntuacion_jugador=0;
+        this.puntuacion_maquina=0;
+    }
+
+}
